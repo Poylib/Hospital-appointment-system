da6da5ca73982b7cc7db73a4b282fa3a455a03aa
poylib <poy_lib@naver.com>
2022-10-17 19:31:01 +0900

    Add 홈화면 레이아웃 구성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,55 +1,120 @@
 import { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Header from '../components/header/Header';
-import Check from '../components/main/check';
-import Reservation from '../components/main/reservaion';
 import { maincolor } from '../theme';
+
 const Home = () => {
-  const openingRef = useRef();
-  const [FadeTime, setFadeTime] = useState(false);
-  useEffect(() => {});
+  const [openFade, setOpenFade] = useState(false);
+  const [openMain, setOpenMain] = useState(false);
+  const [reservationHover, setReservationHover] = useState(false);
+  const [checkHover, setCheckHover] = useState(false);
+  const loadTime = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    setOpenFade(true);
+    loadTime.current = setTimeout(() => {
+      setOpenMain(true);
+    }, 750);
+  }, []);
+
   return (
     <>
       <Header />
       <StyledHome>
-        <OpenFade />
-        <div className='main-box reservation-box'>
-          <h1>예약하기</h1>
-          <div>
-            <span>원하는 날짜와 시간을 정해보세요</span>
-          </div>
-        </div>
-        <div className='main-box check-box'>
-          <h1>예약조회</h1>
-          <div>
-            <span>로그인 또는 예약정보로 예약 목록을 조회할 수 있습니다</span>
-          </div>
-        </div>
+        <OpenFade openFade={openFade} />
+        {openMain && (
+          <>
+            <div
+              className='main-box reservation-box' //
+              onMouseEnter={() => setReservationHover(true)}
+              onMouseLeave={() => setReservationHover(false)}
+            >
+              {reservationHover ? (
+                <div className='hover-box'>
+                  <h1>진료</h1>
+                  <h1>검진</h1>
+                  <h1>상담</h1>
+                </div>
+              ) : (
+                <>
+                  <h1>예약하기</h1>
+                  <div>
+                    <span>원하는 날짜와 시간을 정해보세요</span>
+                  </div>
+                </>
+              )}
+            </div>
+            <div
+              className='main-box check-box' //
+              onMouseEnter={() => setCheckHover(true)}
+              onMouseLeave={() => setCheckHover(false)}
+            >
+              {checkHover ? (
+                <div className='hover-box'>
+                  <h1>예약자명</h1>
+                  <h1>예약 번호</h1>
+                </div>
+              ) : (
+                <>
+                  <h1>예약조회</h1>
+                  <div>
+                    <span>로그인 또는 예약정보로 예약 목록을 조회할 수 있습니다</span>
+                  </div>
+                </>
+              )}
+            </div>
+          </>
+        )}
       </StyledHome>
     </>
   );
 };
+
 export default Home;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
 const StyledHome = styled.div`
   position: relative;
   width: 100%;
   display: flex;
-  flex-direction: row;
   height: 100vh;
+  overflow-x: hidden;
 
   .main-box {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
     height: 100%;
+
     h1 {
+      animation: ${fadeIn} 1.4s;
       font-size: 100px;
       padding-bottom: 40px;
     }
+    span {
+      animation: ${fadeIn} 1.4s;
+      font-size: 25px;
+    }
+    .hover-box {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      height: 100vh;
+      &:hover {
+        cursor: pointer;
+      }
+    }
   }
+
   .reservation-box {
     width: 50%;
     color: ${maincolor};
@@ -61,23 +126,22 @@ const StyledHome = styled.div`
     color: white;
   }
   @media screen and (max-width: 875px) {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
     .main-box {
       h1 {
-        font-size: 20px;
+        font-size: 40px;
       }
     }
   }
 `;
 
-const OpenFade = styled.div`
+const OpenFade = styled.div<{ openFade: boolean }>`
   position: absolute;
-  left: 50%;
+  left: 100%;
   top: 0;
-  z-index: 15;
+  z-index: 10;
   width: 100%;
   height: 100%;
   background-color: ${maincolor};
+  transition: 1.2s ease-in;
+  transform: translateX(${({ openFade }) => (openFade ? '0%' : '-100%')});
 `;
