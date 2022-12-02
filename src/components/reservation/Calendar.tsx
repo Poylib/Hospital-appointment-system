dbf06a58a8b3fc9f00fa4e6061adcf79bb9453a7
poylib <poy_lib@naver.com>
2022-12-03 01:02:07 +0900

    Fix 대소문자 변경 구분 및 firebase 추가

---
+++ src/components/reservation/Calendar.tsx
@@ -0,0 +1,130 @@
+import DatePicker from 'react-datepicker';
+import { useState } from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale';
+import styled from 'styled-components';
+import TimeTable from './TimeTable';
+import axios from 'axios';
+import { maincolor } from '../../theme';
+
+const Calendar = () => {
+  const [schedule, setSchedule] = useState([]);
+  const [startDate, setStartDate] = useState();
+  const [selected, setSelected] = useState('');
+  const today = new Date();
+  const dateHandler = async (e: Date | any) => {
+    const checkedDate = e.toLocaleDateString().split('/').join('-');
+    setStartDate(e);
+    setSelected(checkedDate);
+    try {
+      const { data } = await axios.get('http://localhost:4000/schedule');
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].date === checkedDate) {
+          setSchedule(data[i].booked);
+          break;
+        } else {
+          setSchedule([]);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <StyledCalendar>
+      <CalendarBox>
+        <DatePicker //
+          onChange={dateHandler}
+          dateFormatCalendar='yyyy년 M월'
+          showPreviousMonths={false}
+          useShortMonthInDropdown={false}
+          locale={ko}
+          minDate={today.setDate(today.getDate() + 1)}
+          selected={startDate}
+          inline
+          disabledKeyboardNavigation
+        />
+      </CalendarBox>
+      <TimeTable schedule={schedule} setSelected={setSelected} selected={selected} />
+    </StyledCalendar>
+  );
+};
+export default Calendar;
+
+const StyledCalendar = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  min-width: 550px;
+  height: 70vh;
+  .react-datepicker {
+    width: 100%;
+    padding: 30px 20px;
+    border: none;
+    border-right: 1px solid #eaeaea;
+    font-size: 1rem;
+    color: #606060;
+    .react-datepicker__current-month {
+      padding: 10px;
+      font-size: 20px;
+      color: white;
+    }
+    .react-datepicker__navigation--previous {
+      left: 20px;
+      top: 50%;
+    }
+    .react-datepicker__navigation--next {
+      right: 20px;
+      top: 50%;
+    }
+    .react-datepicker__month-container {
+      width: 100%;
+      .react-datepicker__header {
+        width: 100%;
+        background-color: ${maincolor};
+        border: none;
+        .react-datepicker__current-month {
+          margin-bottom: 10px;
+        }
+        .react-datepicker__day-names {
+          margin: 0;
+          .react-datepicker__day-name {
+            margin: 3%;
+            color: white;
+            font-weight: 600;
+            font-size: 20px;
+          }
+        }
+      }
+      .react-datepicker__month {
+        // 날짜 숫자 클래스
+        .react-datepicker__day {
+          margin: 3%;
+          /* color: blue; */
+          font-weight: 600;
+          font-size: 20px;
+          color: #606060;
+        }
+        //주말 클래스
+        .react-datepicker__day--weekend {
+          color: #e83667;
+        }
+        //오늘 날짜 이전, 선택불가 날짜 클래스
+        .react-datepicker__day--disabled {
+          color: #cacaca;
+        }
+        //시작 날짜, 종료 날짜 사이 중간날짜 선택완료
+        .react-datepicker__day--selected {
+          background-color: ${maincolor};
+          color: white;
+        }
+      }
+    }
+  }
+`;
+
+const CalendarBox = styled.div`
+  width: 50%;
+  min-width: 480px;
+`;
---
+++ src/components/reservation/TimeTable.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { maincolor } from '../../theme';
+import { TimeProps } from '../../util/interface';
+const TimeTable = ({ schedule, setSelected, selected }: TimeProps) => {
+  const arr = ['10 : 00', '11 : 00', '12 : 00', '13 : 00', '14 : 00', '15 : 00', '16 : 00', '17 : 00', '18 : 00'];
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const [timeLine, setTimeLine] = useState(arr);
+
+  useEffect(() => {
+    const newTimeLine = arrArrangeFunc(arr);
+    setTimeLine(newTimeLine);
+  }, [schedule]);
+
+  const clickHandle: React.MouseEventHandler<HTMLLIElement> = e => {
+    if (e.target instanceof HTMLElement) {
+      const inputSelect = `${selected}-${params.id}-${e.target.innerHTML.split(' : ')[0]}`;
+      setSelected(inputSelect);
+      const targetNum: number = Number(e.target.id);
+      const newArr = arrArrangeFunc(arr);
+      newArr[targetNum] = '예약하기';
+      setTimeLine(newArr);
+      if (e.target.innerHTML === '예약하기') {
+        navigate(`/infoinput/${selected}`);
+      }
+    }
+  };
+
+  const arrArrangeFunc = (arr: string[]) => {
+    const newArr = [...arr];
+    for (let i = arr.length - 1; i > 0; i--) {
+      for (let j = 0; j < schedule.length; j++) {
+        if (arr[i].includes(schedule[j].time)) {
+          newArr.splice(i, 1);
+        }
+      }
+    }
+    return newArr;
+  };
+
+  return (
+    <StyledTimeTable>
+      {timeLine.map((time, index) => {
+        return (
+          <li //
+            key={`${time}_${index}`}
+            id={`${index}`}
+            onClick={clickHandle}
+          >
+            {time}
+          </li>
+        );
+      })}
+    </StyledTimeTable>
+  );
+};
+
+export default TimeTable;
+
+const StyledTimeTable = styled.ul`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  min-width: 140px;
+  height: 80%;
+  max-height: 700px;
+  margin-left: 15px;
+  overflow: scroll;
+  li {
+    width: 100%;
+    height: 50px;
+    margin: 10px 0;
+    padding: 10px;
+    text-align: center;
+    font-size: 30px;
+    &:hover {
+      border-radius: 10px;
+      color: white;
+      background-color: ${maincolor};
+      cursor: pointer;
+    }
+  }
+`;
